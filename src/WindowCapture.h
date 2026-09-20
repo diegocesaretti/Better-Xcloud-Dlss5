3b24d999753ceeb05832306710b490daf0577571
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-09-20 18:47:56 -0300

    fix: include WinRT Foundation projections

--- src/WindowCapture.h
+++ src/WindowCapture.h
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include <winrt/base.h>
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Graphics.Capture.h>
 #include <winrt/Windows.Graphics.DirectX.Direct3D11.h>
 
