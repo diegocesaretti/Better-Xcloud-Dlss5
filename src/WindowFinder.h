ad3ea6a9be2065e488a95330e53b59fc9c82b04d
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-09-21 10:48:35 -0300

    capture: add Xbox App target selection

--- src/WindowFinder.h
+++ src/WindowFinder.h
@@ -3,12 +3,21 @@
 #include <windows.h>
 #include <string>
 
+enum class CaptureTargetPreference {
+    XboxApp,
+    Browser,
+    Auto
+};
+
 struct BrowserWindowInfo {
     HWND hwnd{};
     std::wstring processName;
     std::wstring title;
     RECT visualBounds{};
+    CaptureTargetPreference kind{CaptureTargetPreference::Auto};
 };
 
+BrowserWindowInfo FindBestTargetWindow(CaptureTargetPreference preference);
 BrowserWindowInfo FindBestXCloudWindow();
+BrowserWindowInfo FindBestXboxAppWindow();
 bool GetVisualWindowBounds(HWND hwnd, RECT& bounds);
